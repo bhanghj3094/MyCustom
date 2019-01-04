656d6aecdae8217ff3f47b48746098825c5b67cd
bhanghj3094 <bhanghj3094@gmail.com>
2019-01-04 13:49:32 +0900

    LoginActivity created

---
+++ app/src/main/java/com/example/q/mycustom/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.q.mycustom;
+
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+
+        import com.facebook.FacebookSdk;
+        import com.facebook.appevents.AppEventsLogger;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
